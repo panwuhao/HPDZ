2ea6196d5cab23b032bdabe30218101bd366e710
MrLiker <1486358520@qq.com>
2019-12-18 11:39:11 +0800

    cs

---
+++ XIT.MVC.Web/XIT.MVC.Web/Controllers/CustomAuthenticationFilter.cs
@@ -0,0 +1,75 @@
+﻿
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Filters;
+using System.Web.Routing;
+using Newtonsoft.Json;
+using System;
+using XIT.MVC.Web.Common;
+using XIT.EF.Dal;
+using HPIT.Data.Core;
+using System.Web.Security;
+
+namespace MVCLearn.Filters
+{
+    public class CustomAuthenticationFilter : IAuthenticationFilter
+    {
+        public void OnAuthentication(AuthenticationContext filterContext)
+        {
+            //如果action 和 controller的添加的特性里面包含匿名的，则直接过滤掉。
+            if (filterContext.ActionDescriptor.IsDefined(typeof(AllowAnonymousAttribute), inherit: true)
+                || filterContext.ActionDescriptor.ControllerDescriptor.IsDefined(typeof(AllowAnonymousAttribute), inherit: true))
+            {
+                return;
+            }
+            var Url = new UrlHelper(filterContext.RequestContext);
+            var urlstr = Url.Action("Index", "Login");
+            //filterContext.Result = new RedirectResult(urlstr); //指定返回重定向登录界面
+            string cookieName = FormsAuthentication.FormsCookieName;
+            // HttpCookie cokie = filterContext.HttpContext.Request.Cookies.Get("Login");
+            HttpCookie cokie = filterContext.HttpContext.Request.Cookies[cookieName];
+            if (cokie == null)
+            {
+                filterContext.Result = new RedirectToRouteResult(new RouteValueDictionary
+                                       {
+                                           {"controller","Login"},
+                                           {"action","Index"},
+                                           {"returnUrl",filterContext.HttpContext.Request.RawUrl }
+                                       });
+            }
+            else
+            {
+                FormsAuthenticationTicket authTicket = null;
+                try
+                {
+                    authTicket = FormsAuthentication.Decrypt(cokie.Value);
+                }
+                catch (Exception ex)
+                {
+
+                    LogHelper.Default.WriteError(ex.Message);
+                }
+                //var value = System.Text.Encoding.UTF8.GetString(Convert.FromBase64String(cokie.Value));
+                if (authTicket!=null)
+                {
+                    DeluxeUser.CurrentMember = JsonConvert.DeserializeObject<LoginInfo>(authTicket.UserData);
+
+                }
+
+            }
+        }
+
+        public void OnAuthenticationChallenge(AuthenticationChallengeContext filterContext)
+        {
+            if (filterContext.Result == null || filterContext.Result is HttpUnauthorizedResult)
+            {
+                filterContext.Result = new RedirectToRouteResult(new RouteValueDictionary
+                                       {
+                                           {"controller","Login"},
+                                           {"action","Index"},
+                                           {"returnUrl",filterContext.HttpContext.Request.RawUrl }
+                                       });
+            }
+        }
+    }
+}
---
+++ XIT.MVC.Web/XIT.MVC.Web/Controllers/FileUploadController.cs
@@ -0,0 +1,85 @@
+﻿using HPIT.Data.Core;
+using HPIT.Survey.Portal.Filters;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using XIT.MVC.Web.Models;
+
+namespace XIT.MVC.Web.Controllers
+{
+    public class FileUploadController : Controller
+    {
+        // GET: FileUpload
+        [AllowAnonymous]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [AllowAnonymous]
+        //[HttpPost]
+        public ActionResult ProcessFileLoad(IEnumerable<HttpPostedFileBase> filenames)
+        {
+            foreach (var file in filenames)
+            {
+                var filename = Path.GetFileName(file.FileName);
+                var path = Path.Combine(Server.MapPath("~/Upload"), filename);
+                file.SaveAs(path);
+            }
+            return RedirectToAction("FileListIndex");
+        }
+        [AllowAnonymous]
+        public FileResult download(string filename)
+
+        {
+            string filePath = Server.MapPath("~/Upload/" + filename);
+            return File(filePath, "text/plain", filename);
+        }
+        [AllowAnonymous]
+        public JsonResult GetFilelist(SearchModel<FileModel> search)
+        {
+            int total = 0;
+            string filePath = Server.MapPath("~/Upload/");//路径
+            DirectoryInfo directoryInfo = new DirectoryInfo(filePath);
+            //获取文件夹下的所有文件
+            FileInfo[] allfiles = directoryInfo.GetFiles();
+            //生成新的文件类型的数据集合
+            var data = from file in allfiles
+                       select new
+                       {
+                           filename = file.Name,
+                           path = file.DirectoryName,
+                           time = file.LastWriteTime,
+                           fullname = file.FullName
+                       };
+            //
+            total = allfiles.Length;
+            var totalPages = total % search.PageSize == 0 ? total / search.PageSize : (total / search.PageSize) + 1;
+            JsonResult result = new JsonResult();
+            //原生分页代码skip take
+            var pagedata = data.Skip((search.PageIndex - 1) * search.PageSize).Take(search.PageSize);
+            //组织新的数据Data，Total, TotalPages
+            //result.Data = new
+            //{
+            //    Data = pagedata,
+            //    Total = total,
+            //    TotalPages = totalPages
+            //};
+            result.JsonRequestBehavior = JsonRequestBehavior.AllowGet;
+            //return result;
+            return new DeluxeJsonResult(new
+            {
+                Data = pagedata,
+                Total = total,
+                TotalPages = totalPages
+            }, "yyyy-MM- dd HH: mm");
+        }
+        [AllowAnonymous]
+        public ActionResult FileListIndex()
+        {
+            return View();
+        }
+    }
+}
